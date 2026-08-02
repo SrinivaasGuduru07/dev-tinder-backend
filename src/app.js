59f64a1e32488240722dbf2501f442f453724bd5
SrinivaasGudur07 <srinivaasguduru@gmail.com>
2026-08-03 01:18:23 +0530

    Ep-9 encryption

--- src/app.js
+++ src/app.js
@@ -2,14 +2,32 @@ const express = require("express");
 const connectDB = require("./config/database");
 const app = express();
 const User = require("./models/user");
+const { validateSignUpData } = require("./utils/validation");
+
+const bcrypt = require("bcrypt");
 
 app.use(express.json());
 
 app.post("/signup",async (req,res)=>{
-//creating a new instance of the User Model.
-    const user = new User(req.body);
- 
+
 try{
+//validation of data.
+validateSignUpData(req);
+
+//encrypt the user password.
+const{firstName,lastName,emailId,password}=req.body;
+
+const passwordHash = await bcrypt.hash(password,10);
+
+
+
+
+
+
+//creating a new instance of the User Model.
+    const user = new User({firstName,lastName,emailId,password:passwordHash});
+ 
+
        await user.save();
    res.send("User added successfully!!!");
 }
@@ -20,30 +38,31 @@ catch(err){
 
 })
 
-//get one user from the database:
-app.get("/user", async (req,res)=>{
-    const userEmail = req.body.emailId;
+app.post("/login",async(req,res)=>{
+
     try{
-        const users = await User.find({emailId:userEmail});
-        if(users.length === 0){
-            res.status(404).send("User not Found");
+        const{emailId,password}=req.body;
+        const user = await User.findOne({emailId:emailId});
+
+        if(!user){
+            throw new Error("Invalid credentials")
+        }
+
+        const isPasswordValid = await bcrypt.compare(password,user.password);
+
+        if(isPasswordValid){
+            res.send("Login Successful!!");
         }else{
-            res.send(users)
+            throw new Error("Invalid credentials")
         }
     }
     catch(err){
-    console.error("Something went Wrong!!!");
+    res.status(400).send("Error saving the user:"+err.message);
 }
+
 })
-//Get all the users form the database:
-app.get("/feed",async(req,res)=>{
-    try{
-        const users = await User.find({});
-        res.send(users);
-    }catch(err){
-        res.status(400).send("Something went wrong!!")
-    }
-})
+
+
 connectDB().then(()=>{
     console.log("Database Connected Successfully!!!");
 
